485524f2f95bb2ca6a377b4c12d498f1c9e93275
Vedran Blazenka <blazenka.vedran@gmail.com>
2016-11-16 17:33:09 +0100

    Added logo

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <div>
+      <h1 className="page-title">Lorem Trumpsum</h1>
+      <div className="trump-logo">
+      </div>
+    </div>
+  )
+};
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 // Components
 import Quote from '../components/Quote';
+import Logo from '../components/Logo';
 import Form from './Form';
 import PlaceholderText from './PlaceholderText';
 
 const Home = (props) => (
   <section {...props} className="home">
     <div className="container">
+      <Logo />
       <Quote />
 
       <Form />
